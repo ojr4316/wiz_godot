dad36881415d9ee9080470e66bba8798e2491869
Owen <ojr4316@rit.edu>
2023-09-19 01:08:47 -0400

    basic particles, swipe detection

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,58 @@
 extends CharacterBody3D
 
-@export var speed := 10
-@export var jumpStrength := 15
-@export var fall_acceleration = 75
+@export var speed := 8
+@export var jumpStrength := 12
+@export var fall_acceleration = 50
 
 var target_velocity = Vector3.ZERO
+var ball: PackedScene
+
+signal swipe
+var swipe_start: Vector2
+var min_drag := 100
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("im gonna preeeeeeee");
+	print("im gonna preeeeeeee")
+	ball = load("res://scenes/ball.tscn")
+	print(ball)
 	
+	swipe.connect(trySwipe)
+	
+func trySwipe(e):
+	print(e)
+	var b = ball.instantiate()
+	get_parent().add_child(b)
+	b.global_position = Vector3(position.x, position.y, position.z-1)
+	match e:
+		"left":
+			(b as RigidBody3D).apply_impulse(Vector3(-5, 1, 0))
+		"right":
+			(b as RigidBody3D).apply_impulse(Vector3(5, 1, 0))
 
+var drag: bool = false
 
+func calculate_swipe(swipe_end):
+	if swipe_start == null: 
+		return
+	var s = swipe_end - swipe_start
+	print(s)
+	if abs(s.x) > min_drag:
+		if s.x > 0:
+			swipe.emit("right")
+			#emit_signal("swipe", "right")
+		else:
+			swipe.emit("left")
+			
 func _input(event):
+	if event.is_action_pressed("interact"):
+		swipe_start = event.position
+	if event.is_action_released("interact"):
+		calculate_swipe(event.position)
+			
 	if event.is_action_pressed("jump") && is_on_floor():
-		print("jump")
-		target_velocity.y = jumpStrength;
-		
-		
+		target_velocity.y = jumpStrength
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
@@ -48,5 +83,5 @@ func _physics_process(delta):
 	move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
